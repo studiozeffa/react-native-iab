18254c20ffb2ae781ec16d006d5656603d9b035d
Tom Spencer <tom.spencer@studiozeffa.com>
2018-01-18 22:58:16 +0000

    Better iOS availability detection

--- iab.ios.js
+++ iab.ios.js
@@ -3,15 +3,19 @@ import SafariView from 'react-native-safari-view';
 
 export function openBrowser(url, options = {}) {
   // TODO onShow/onHide callbacks
-  return SafariView.show({
-    ...(options.ios || {}),
-    url,
-  }).catch(e => {
-    if (!options.noExternal) {
-      return Linking.openURL(url);
-    }
-    throw e;
-  });
+  SafariView.isAvailable()
+    .then(() =>
+      SafariView.show({
+        ...(options.ios || {}),
+        url,
+      }),
+    )
+    .catch(e => {
+      if (!options.noExternal) {
+        return Linking.openURL(url);
+      }
+      throw e;
+    });
 }
 
 export function closeBrowser() {
